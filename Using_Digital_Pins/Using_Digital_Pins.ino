1e28bd64bc8a682d2bad7a44f217ac931c7a6139
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-07-21 16:54:25 +0300

    Using Digital Pins

--- Using_Digital_Pins/Using_Digital_Pins.ino
+++ Using_Digital_Pins/Using_Digital_Pins.ino
@@ -1,14 +1,20 @@
+//Using a push button to switch off the LED when pressed and on when released.
+
+//A small LED Bulb
 int ledPin = 3;
+//A push button
 int buttonPin = 2;
 
 void setup() {
-  // put your setup code here, to run once:
+  //Assign the variable to a pin on the arduino board
   pinMode(ledPin, OUTPUT);
+  //Instead of connecting a pullup resistor when can assign it here 
   pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Initialize the state of button and it will be digitally read from the buttonPin variable
   int buttonState = digitalRead(buttonPin);
+  //A value of 1 or 0 will be assigned to the pin of the led light
   digitalWrite(ledPin, buttonState);
 }
