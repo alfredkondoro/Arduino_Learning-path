840d9cc7b1c553d89b3b1d26fcfc2358ed7c91d7
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-07-21 15:41:04 +0300

    Using Variables

---
+++ Variables/Variables.ino
@@ -0,0 +1,19 @@
+//variables are created at the top of the sketch
+//data_type variable_name = value;
+
+int switchState;
+//you can declare and define it
+//default value of undefined is zero
+int ledPin = 13;
+//Variables can be Global as defined above or can be local
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //An example of local variable
+  int sensorValue = 17;
+}
